dc1c9cff43f405ed5a57662cb1d21ad4813e79e4
breadscorner <hello@brettgill.io>
2024-03-16 20:20:14 -0700

    add footer

--- obj/Debug/net8.0/book-rating.AssemblyInfo.cs
+++ obj/Debug/net8.0/book-rating.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("book-rating")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8081b05e11cce77b2d9963bc4c31d6d7b5c6a794")]
 [assembly: System.Reflection.AssemblyProductAttribute("book-rating")]
 [assembly: System.Reflection.AssemblyTitleAttribute("book-rating")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
